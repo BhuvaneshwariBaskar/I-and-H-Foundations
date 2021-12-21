87b7a5fbf96a2301a21e88e5ff478f9b92077638
sai aravind <saiaravind9840@gmail.com>
2021-12-21 18:32:05 +0530

    byAravind

---
+++ index.js
@@ -0,0 +1,88 @@
+// window.prompt("yea its working !");
+
+const div1=document.createElement("div");
+const ul1=document.createElement("ul");
+const li0=document.createElement("li");
+const li1=document.createElement("li");
+const li2=document.createElement("li");
+const li3=document.createElement("li");
+const li4=document.createElement("li");
+const li5=document.createElement("li");
+const li6=document.createElement("li");
+const img1=document.createElement("img");
+const img2=document.createElement("img");
+const img3=document.createElement("img");
+const div2=document.createElement("div");
+const div11=document.createElement("div");
+const div12=document.createElement("div");
+const div21=document.createElement("div");
+const div22=document.createElement("div");
+const div23=document.createElement("div");
+const div23div=document.createElement("div");
+const img4=document.createElement("img");
+const img4div=document.createElement("div");
+img4.src="img4.png";
+img1.src="img1.png";
+img2.src="img2.png";
+img3.src="img3.png";
+
+
+li1.innerHTML="<b>HOME</b>";
+li2.innerHTML="<b>ABOUT US</b>";
+li3.innerHTML="<b>GALARY</b>";
+li4.innerHTML="<b>CONTACT US</b>";
+div11.innerHTML="login";
+div12.innerHTML="sign up";
+div22.innerHTML="<br><br><br><br><br><br><br><br><br><b>“HELP” is a very small word that has greater <br> depths in it. Help can be small or big<br> depending on the time it is been given or <br>done. It always gives joy to help others.<br> Mahatma Gandhi once said – The best way to <br>find yourself is to lose yourself in the service <br> of others.</b><br><br><br><br>";
+div21.innerHTML="<br><br><br><br><br><br><br><br><br><br><br><br><div><span>shk </span></div >";
+div23div.innerHTML="<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><div><b>CONTACT US </b></div> ";
+
+document.body.append(div1);
+document.body.append(div2);
+div1.append(ul1);
+ul1.append(li0);
+ul1.append(li1);
+ul1.append(li2);
+ul1.append(li3);
+ul1.append(li4);
+ul1.append(li5);
+ul1.append(li6);
+li0.append(img1);
+div1.append(img2);
+li5.append(div11);
+li6.append(div12);
+div2.append(div21);
+div2.append(div22);
+div2.append(div23);
+div23.append(div23div);
+div23.append(img4div);
+img4div.append(img4);
+
+
+
+
+div1.id="div1";
+ul1.className="ul1";
+li0.id="li0";
+li1.id="li1";
+li2.id="li2";
+li3.id="li3";
+li4.id="li4";
+li5.id="li5";
+li6.id="li6";
+img1.id="img1";
+img2.id="img2";
+div2.className="div2";
+div21.id="div21";
+div22.id="div22";
+img2.id="img2";
+// img3.id="img3";
+div11.id="div11";
+div12.id="div12";
+div23.id="div23";
+img4.id="img4";
+img4div.id="img4div";
+div23div.id="div23div";
+console.log("hello");
+
+
